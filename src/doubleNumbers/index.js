f520ad58729dcba0028c0cc827918bdfe326221f
Olha Holysh <32979019+pinkmadkitten@users.noreply.github.com>
2018-10-31 18:54:14 +0200

    feat: implement methods `sortByWeight` and `doubleNum` (#1)

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,9 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const stringified = num.toString();
+  const halfLength = stringified.length / 2;
+  const firstHalf = stringified.slice(0, halfLength);
+  const secondHalf = stringified.slice(halfLength);
+
+  return firstHalf === secondHalf ? num : num * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,12 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  const newArray = [];
+  arr
+    .map((item, index) => ({
+      item: item.split('').reduce((acc, next) => acc + parseFloat(next), 0),
+      index
+    }))
+    .sort((a, b) => a.item - b.item)
+    .forEach(item => newArray.push(arr[item.index]));
+  return newArray;
 };
